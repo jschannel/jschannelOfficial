dd54489de2b0d675bb993b67f5ec4e8bba38c898
Mahesh Haldar <haldar.mahesh@gmail.com>
2017-01-16 17:13:16 +0530

    Update dist

--- client/src/components/footer/footer.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('jschannel')
-  .controller('FooterCtrl', function () {
-    
-  });
--- client/src/components/navbar/navbar.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('jschannel')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
